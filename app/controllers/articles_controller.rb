63ee00daa5287285431fbe1fc63956a5540073b4
ramror <ramesh.ramror@gmail.com>
2017-12-10 23:24:38 +0530

    Edit the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,20 @@ class ArticlesController < ApplicationController
 		end 
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end 
+	end
+
 	def show 
 		@article = Article.find(params[:id])
 	end 
