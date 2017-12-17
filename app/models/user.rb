128a332e85c3556e1a0057e65321bffe6854d212
ramror <ramesh.ramror@gmail.com>
2017-12-17 22:23:11 +0530

    user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 	has_many :comments
+	validates :username, presence: true, uniqueness: { case_sesitive: false	}, 
+			   length: { minimum: 3, maximum: 25 }
+    validates :email, presence: true, length: { maximum: 25}
+    		   uniqueness: { case_sesitive: false }
+    		   format: { with: VALID_EMAIL_REGEX }
 end
